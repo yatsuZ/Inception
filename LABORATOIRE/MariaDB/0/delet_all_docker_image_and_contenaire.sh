ea547e6a35d3e904965041e90bb87795f337c45d
y.zaoui.pro@gmail.com <root@localhost.42paris.fr>
2024-10-18 18:52:49 +0200

    Compliquer dinstaller mariadb

--- LABORATOIRE/MariaDB/0/delet_all_docker_image_and_contenaire.sh
+++ LABORATOIRE/MariaDB/0/delet_all_docker_image_and_contenaire.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker stop $(docker ps -q)
-docker rm $(docker ps -aq) 
-docker rmi $(docker images -q) 
+docker stop $(docker ps -q) 2>/dev/null || echo "Aucun conteneur à arrêter."
+docker rm $(docker ps -aq) 2>/dev/null || echo "Aucun conteneur à supprimer."
+docker rmi $(docker images -q) 2>/dev/null || echo "Aucune image à supprimer."
