3f6105d7c7501aecde8e4614977816a34230ebe3
y.zaoui.pro@gmail.com <root@localhost.42paris.fr>
2024-10-18 16:07:20 +0200

    modification du script de creation de contenair

--- LABORATOIRE/MariaDB/0/creat_image_and_contenair_and_exec.sh
+++ LABORATOIRE/MariaDB/0/creat_image_and_contenair_and_exec.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
+#!/bin/sh
+
 docker build -t image_test .
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then  # Vérifie si la commande a échoué (code de retour différent de 0)
     exit 1
 fi
 
-
-echo "Vouloir manipuler directement la vm ? (y/n)"
+echo "Vouloir manipuler directement la VM ? (y/n)"
 read go_in
-echo "run en detache ? (y/n)"
+echo "Run en détaché ? (y/n)"
 read answer
 
 if [ "$answer" = "y" ]; then
-	docker run -d --name contenair_test image_test
-	if ["$go_in" = "y"]; then
-		docker exec -it contenair_test sh sh
-	fi
+    docker run -d --name contenair_test image_test
+    if [ "$go_in" = "y" ]; then
+        docker exec -it contenair_test sh  # Lancement d'un shell interactif dans le conteneur
+    fi
 else
-	if ["$go_in" = "y"]; then
-		docker run -it --name contenair_test image_test
-	else
-		docker run --name contenair_test image_test
-	fi
+    if [ "$go_in" = "y" ]; then
+        docker run -it --name contenair_test image_test
+    else
+        docker run --name contenair_test image_test
+    fi
 fi
+
