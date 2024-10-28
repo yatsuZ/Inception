cb2910270e84ecbdda0089a6d3b657f1e92851d7
yatsuZ <y.zaoui.pro@gmail.com>
2024-10-28 14:37:39 +0100

    Derniere version de WP

---
+++ LABORATOIRE/WordPress/4/creat_image_and_contenair_and_exec.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#!/bin/sh
+
+docker build -t image_test .
+if [ $? -ne 0 ]; then  # Vérifie si la commande a échoué (code de retour différent de 0)
+    exit 1
+fi
+
+echo "Vouloir manipuler directement la VM ? (y/n)"
+read go_in
+echo "Run en détaché ? (y/n)"
+read answer
+
+if [ "$answer" = "y" ]; then
+    docker run -d --name contenair_test image_test
+    if [ "$go_in" = "y" ]; then
+        docker exec -it contenair_test sh  # Lancement d'un shell interactif dans le conteneur
+    fi
+else
+    if [ "$go_in" = "y" ]; then
+        docker run -it --name contenair_test image_test
+    else
+        docker run --name contenair_test image_test
+    fi
+fi
+
---
+++ LABORATOIRE/WordPress/4/start_wp.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+# Attendre 10 secondes pour s'assurer que MariaDB est démarré
+sleep 10
+
+# Chemin vers le répertoire WordPress
+WP_PATH="/var/www/html"
+
+# Vérifier si le fichier wp-config.php existe déjà pour déterminer si WordPress est installé
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+    echo "Installation de WordPress..."
+
+    # Créer le répertoire de destination s'il n'existe pas
+    mkdir -p "$WP_PATH"
+    
+    # Télécharger et extraire WordPress
+    wget https://wordpress.org/latest.tar.gz -P /tmp
+    tar -xzf /tmp/latest.tar.gz -C /tmp
+    mv /tmp/wordpress/* "$WP_PATH"
+    rm -rf /tmp/latest.tar.gz /tmp/wordpress
+
+    # Créer le fichier wp-config.php avec WP-CLI
+    wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="localhost" --path="$WP_PATH"
+    
+    # Installer WordPress avec WP-CLI
+    wp core install --url="http://example.com" --title="Mon Site WordPress" --admin_user="admin" --admin_password="admin_password" --admin_email="email@example.com" --path="$WP_PATH"
+
+    echo "WordPress installé avec succès."
+else
+    echo "WordPress est déjà installé."
+fi
+
+# Installation et activation des plugins, si nécessaire
+wp plugin install wp-redis --activate --path="$WP_PATH"
+wp redis enable --path="$WP_PATH"
+
+# Démarrer PHP-FPM
+exec php-fpm82 -F
