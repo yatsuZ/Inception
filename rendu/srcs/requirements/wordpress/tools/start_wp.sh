94798d8c636aabb918fd0a20921a0c93766fc7ee
y.zaoui.pro@gmail.com <root@localhost.42paris.fr>
2024-11-06 19:54:41 +0100

    resoudre le probleme de wp qui ne so conecte pas a mariadb sinon ca va sa amrche

--- rendu/srcs/requirements/wordpress/tools/start_wp.sh
+++ rendu/srcs/requirements/wordpress/tools/start_wp.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 
-sleep 15
+# Définition des couleurs
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
 
-# WP_PATH="/var/www/wordpress"
-
-echo "Vérification de l'installation de WordPress..."
+echo "${BLUE}Vérification de l'installation de WordPress...${RESET}"
 
 # Vérifier si le répertoire de WordPress existe
 if [ ! -d "$WP_PATH" ] || [ ! -f "$WP_PATH/wp-config.php" ]; then
-    echo "Installation de WordPress..."
+    echo "${YELLOW}Installation de WordPress...${RESET}"
 
     mkdir -p "$WP_PATH"
     chown -R root:root "$WP_PATH"
@@ -21,24 +24,37 @@ if [ ! -d "$WP_PATH" ] || [ ! -f "$WP_PATH/wp-config.php" ]; then
 
     # Créer le fichier wp-config.php si nécessaire
     if [ ! -f "$WP_PATH/wp-config.php" ]; then
-        wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb" --path="$WP_PATH"
 
-        # Installer WordPress avec WP-CLI
+        sleep 5
+
+        # Attendre que la base de données soit prête
+        until mysql -h "$SQL_HOST" -u "$SQL_NAME_USER" -p"$SQL_PASSWORD_USER" -e "SHOW DATABASES;" > /dev/null 2>&1; do
+            echo "${YELLOW}En attente de la base de données...${RESET}"
+            echo "${YELLOW}Essai de connexion à : $SQL_HOST avec l'utilisateur : $SQL_NAME_USER${RESET}"
+            sleep 2
+        done
+
+        echo "${GREEN}Connexion à la base de données réussie${RESET}"
+        
+        # Utilisation de wp-cli pour créer le fichier wp-config.php
+        wp config create --dbname="$SQL_NAME_DATABASE" --dbuser="$SQL_NAME_USER" --dbpass="$SQL_PASSWORD_USER" --dbhost="$SQL_HOST" --path="$WP_PATH"
+
+        # Installation de WordPress avec WP-CLI
         # wp core install --url="http://example.com" --title="Mon Site WordPress" --admin_user="admin" --admin_password="admin_password" --admin_email="email@example.com" --path="$WP_PATH"
 
-        echo "Fichier wp-config.php créé et WordPress installé avec succès."
-#        echo "Fichier wp-config.php doit etre créé."
+        # Installation et activation des plugins, si nécessaire
+        # wp plugin install wp-redis --activate --path="$WP_PATH"
+        # wp redis enable --path="$WP_PATH"
+
+        echo "${GREEN}Fichier wp-config.php créé et WordPress installé avec succès.${RESET}"
     else
-        echo "WordPress est déjà installé, fichier wp-config.php trouvé."
+        echo "${GREEN}WordPress est déjà installé, fichier wp-config.php trouvé.${RESET}"
     fi
 else
-    echo "WordPress est déjà installé, répertoire et wp-config.php trouvés."
+    echo "${GREEN}WordPress est déjà installé, répertoire et wp-config.php trouvés.${RESET}"
 fi
 
-# Installation et activation des plugins, si nécessaire
-# wp plugin install wp-redis --activate --path="$WP_PATH"
-# wp redis enable --path="$WP_PATH"
-
 # Démarrer PHP-FPM
+echo "${BLUE}Démarrage de PHP-FPM...${RESET}"
 exec php-fpm82 -F
 
