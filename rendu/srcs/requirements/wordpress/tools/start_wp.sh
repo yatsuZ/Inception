b5bc151b81d5b4c7740e8bbb8ded8725549f99f6
y.zaoui.pro@gmail.com <root@localhost.42paris.fr>
2024-11-12 20:07:11 +0100

    derniere ligne droite

--- rendu/srcs/requirements/wordpress/tools/start_wp.sh
+++ rendu/srcs/requirements/wordpress/tools/start_wp.sh
@@ -7,11 +7,11 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 RESET='\033[0m'
 
-echo "${BLUE}Vérification de l'installation de WordPress...${RESET}"
+echo -e "${BLUE}Vérification de l'installation de WordPress...${RESET}"
 
 # Vérifier si le répertoire de WordPress existe
 if [ ! -d "$WP_PATH" ] || [ ! -f "$WP_PATH/wp-config.php" ]; then
-    echo "${YELLOW}Installation de WordPress...${RESET}"
+    echo -e "${YELLOW}Installation de WordPress...${RESET}"
 
     mkdir -p "$WP_PATH"
     chown -R root:root "$WP_PATH"
@@ -25,19 +25,19 @@ if [ ! -d "$WP_PATH" ] || [ ! -f "$WP_PATH/wp-config.php" ]; then
     # Créer le fichier wp-config.php si nécessaire
     if [ ! -f "$WP_PATH/wp-config.php" ]; then
 
-        sleep 5
 
         # Attendre que la base de données soit prête
         until mysql -h "$SQL_HOST" -u "$SQL_NAME_USER" -p"$SQL_PASSWORD_USER" -e "SHOW DATABASES;" > /dev/null 2>&1; do
-            echo "${YELLOW}En attente de la base de données...${RESET}"
-            echo "${YELLOW}Essai de connexion à : $SQL_HOST avec l'utilisateur : $SQL_NAME_USER${RESET}"
+            echo -e "${YELLOW}En attente de la base de données...${RESET}"
+            echo -e "${YELLOW}Essai de connexion à : $SQL_HOST avec l'utilisateur : $SQL_NAME_USER${RESET}"
             sleep 2
         done
 
-        echo "${GREEN}Connexion à la base de données réussie${RESET}"
+        echo -e "${GREEN}Connexion à la base de données réussie${RESET}"
         
         # Utilisation de wp-cli pour créer le fichier wp-config.php
-        wp config create --dbname="$SQL_NAME_DATABASE" --dbuser="$SQL_NAME_USER" --dbpass="$SQL_PASSWORD_USER" --dbhost="$SQL_HOST" --path="$WP_PATH"
+#        wp config create --dbname="$SQL_NAME_DATABASE" --dbuser="$SQL_NAME_USER" --dbpass="$SQL_PASSWORD_USER" --dbhost="$SQL_HOST" --path="$WP_PATH"
+#		sed "s/username_here/$SQL_NAME_USER/;s/password_here/$SQL_PASSWORD/;s/REDIS_PASSWORD/$REDIS_PASSWORD/;" /etc/wp-config-sample.php > wp-config.php
 
         # Installation de WordPress avec WP-CLI
         # wp core install --url="http://example.com" --title="Mon Site WordPress" --admin_user="admin" --admin_password="admin_password" --admin_email="email@example.com" --path="$WP_PATH"
@@ -46,15 +46,15 @@ if [ ! -d "$WP_PATH" ] || [ ! -f "$WP_PATH/wp-config.php" ]; then
         # wp plugin install wp-redis --activate --path="$WP_PATH"
         # wp redis enable --path="$WP_PATH"
 
-        echo "${GREEN}Fichier wp-config.php créé et WordPress installé avec succès.${RESET}"
+        echo -e "${GREEN}Fichier wp-config.php créé et WordPress installé avec succès.${RESET}"
     else
-        echo "${GREEN}WordPress est déjà installé, fichier wp-config.php trouvé.${RESET}"
+        echo -e "${GREEN}WordPress est déjà installé, fichier wp-config.php trouvé.${RESET}"
     fi
 else
-    echo "${GREEN}WordPress est déjà installé, répertoire et wp-config.php trouvés.${RESET}"
+    echo -e "${GREEN}WordPress est déjà installé, répertoire et wp-config.php trouvés.${RESET}"
 fi
 
 # Démarrer PHP-FPM
-echo "${BLUE}Démarrage de PHP-FPM...${RESET}"
+echo -e "${BLUE}Démarrage de PHP-FPM...${RESET}"
 exec php-fpm82 -F
 
