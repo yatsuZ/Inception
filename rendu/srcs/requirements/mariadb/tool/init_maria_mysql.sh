a224785bc5f7bca9c3122002fd7d7b434766b092
YatsuZ <yassine.zaoui28062003@gmail.com>
2024-11-15 23:31:44 +0100

    test

--- rendu/srcs/requirements/mariadb/tool/init_maria_mysql.sh
+++ rendu/srcs/requirements/mariadb/tool/init_maria_mysql.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/env sh
 
-# Afficher les valeurs des variables d'environnement
-echo "SQL_NAME_DATABASE: ${SQL_NAME_DATABASE}"
-echo "SQL_NAME_USER: ${SQL_NAME_USER}"
-echo "SQL_PASSWORD_USER: ${SQL_PASSWORD_USER}"
-echo "SQL_PASSWORD_ROOT: ${SQL_PASSWORD_ROOT}"
+# Définition des couleurs
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 
-if find /var/lib/mysql -mindepth 1 -maxdepth 1 | read; then
-    echo "db exists"
+# Afficher les valeurs des variables d'environnement
+echo -e "${BLUE}MARIA_PATH:${NC} ${MARIA_PATH}"
+echo -e "${BLUE}SQL_NAME_DATABASE:${NC} ${SQL_NAME_DATABASE}"
+echo -e "${BLUE}SQL_NAME_USER:${NC} ${SQL_NAME_USER}"
+echo -e "${BLUE}SQL_PASSWORD_USER:${NC} ${SQL_PASSWORD_USER}"
+echo -e "${BLUE}SQL_PASSWORD_ROOT:${NC} ${SQL_PASSWORD_ROOT}"
+
+# Vérifier si la base de données existe déjà
+if find ${MARIA_PATH} -mindepth 1 -maxdepth 1 | read; then
+    echo -e "${GREEN}La base de données existe déjà.${NC}"
 else
-    echo "need to create db"
+    echo -e "${YELLOW}La base de données doit être créée.${NC}"
     mysql_install_db -umysql --ldata=/var/lib/mysql
     mariadbd -umysql &
     sleep 1
 
-    echo "created by using a script sql"
+    echo -e "${YELLOW}Création de la base de données et de l'utilisateur via un script SQL.${NC}"
     # Exécuter le script SQL avec des variables d'environnement
     envsubst < /bin/init_db.sql | mysql -u root
     if [ $? -ne 0 ]; then
-        echo "Error during database creation"
+        echo -e "${RED}Erreur lors de la création de la base de données.${NC}"
         exit 1
     fi
 
-    echo "Database and user created"
+    echo -e "${GREEN}Base de données et utilisateur créés avec succès.${NC}"
 
     mysqladmin shutdown -p"${SQL_PASSWORD_ROOT}"
 fi
 
+echo -e "${GREEN}Démarrage du serveur MariaDB...${NC}"
 exec mariadbd -umysql
-
-#echo "SELECT User, Host FROM mysql.user;" | mysql -u root -p"${SQL_PASSWORD_ROOT}" -h 127.0.0.1 -P 3306
